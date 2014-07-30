4aef548bd8f9c20fb21e1d754ce1ff5ca69325c0
ThienAu <vinh.thien0301@gmail.com>
2014-07-31 01:37:40 +0700

    fix

---
+++ Youtube Pro 2015/Youtube Pro 2015/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Youtube_Pro_2015.Resources;
+
+namespace Youtube_Pro_2015
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Youtube Pro 2015/Youtube Pro 2015/Views/PageAboutUs.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Utility;
+
+namespace Youtube_Pro_2015.Views
+{
+    public partial class PageAboutUs : PhoneApplicationPage
+    {
+        public PageAboutUs()
+        {
+            InitializeComponent();
+        }
+
+        private void lblRating_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            SystemHelper.RateAndReview();
+        }
+
+        private void lblUpdateVersion_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            SystemHelper.UpdateVersion();
+        }
+
+        private void lblMyLink_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            SystemHelper.OpenLink("http://techstorm-solution.com/");
+        }
+
+        private void lblMyTeam_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            SystemHelper.OpenLink("http://techstorm-solution.com/");
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            lblMyTeam.Text = SystemHelper.GetAppAuthor();
+        }
+
+    }
+}
